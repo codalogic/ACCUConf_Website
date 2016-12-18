b07fae30a407bbfa6130946d2330bf617e69078e
Russel Winder <russel@winder.org.uk>
2016-12-18 14:45:21 +0000

    A few tinkerings to get the admin interface working. Temporarily suspended as it is not working as it should.

--- flask_application_part/accuconf/proposals/utils/schedule.py
+++ flask_application_part/accuconf/proposals/utils/schedule.py
@@ -2,38 +2,38 @@ from enum import Enum
 
 
 class ConferenceDay(Enum):
-    workshops = 'Workshops Day'
-    day_1 = 'Day 1'
-    day_2 = 'Day 2'
-    day_3 = 'Day 3'
-    day_4 = 'Day 4'
+    workshops = 'workshops'
+    day_1 = 'day_1'
+    day_2 = 'day_2'
+    day_3 = 'day_3'
+    day_4 = 'day_4'
 
 
 class SessionSlot(Enum):
-    session_1 = 'Session 1'
-    session_2 = 'Session 2'
-    session_3 = 'Session 3'
+    session_1 = 'session_1'
+    session_2 = 'session_2'
+    session_3 = 'session_3'
 
 
 class QuickieSlot(Enum):
-    slot_1 = 'Slot 1'
-    slot_2 = 'Slot 2'
-    slot_3 = 'Slot 3'
-    slot_4 = 'Slot 4'
+    slot_1 = 'slot_1'
+    slot_2 = 'slot_2'
+    slot_3 = 'slot_3'
+    slot_4 = 'slot_4'
 
 
 class Track(Enum):
-    cpp = 'C++'
+    cpp = 'cpp'
     other = 'other'
 
 
 class Room(Enum):
-    bristol_suite = 'Bristol Suite'
-    bristol_1 = 'Bristol 1'
-    bristol_2 = 'Bristol 2'
-    bristol_3 = 'Briostol 3'
-    empire = 'Empire Suite'
-    great_britain = 'SS Great Britain Suite'
+    bristol_suite = 'bristol_suite'
+    bristol_1 = 'bristol_1'
+    bristol_2 = 'bristol_2'
+    bristol_3 = 'bristol_3'
+    empire = 'empire'
+    great_britain = 'great_britain'
 
 
 
